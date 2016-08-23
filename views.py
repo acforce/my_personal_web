64c77738fa788a27bba226c6527fbbe08e10aca3
acforce <lee@tamu.edu>
2016-08-22 20:43:00 -0500

    dev2: finished the tags and categories

--- views.py
+++ views.py
@@ -1,13 +1,18 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
-from .models import Blog, Author, Entry
+from .models import *
 # Create your views here.
 
 def index(request):
     latest_entries = Entry.objects.order_by('-pub_time')[0:10]
+    tags = Tag.objects.all();
+    categories = Category.objects.all();
+	
     context = {
         'latest_entries': latest_entries,
+	    'tags': tags,
+	    'categories': categories,
     }
     return render(request, 'weblog/home.html', context)
 
