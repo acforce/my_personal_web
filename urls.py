b30f11a1b290b146cb46b9a85c410a4faa57c66a
acforce <lee@tamu.edu>
2016-09-01 21:25:53 -0500

    support paging

--- urls.py
+++ urls.py
@@ -9,6 +9,8 @@ urlpatterns = [
     url(r'^$', views.index, name='index'),
     url(r'^(?P<year>\d{4})/(?P<month>\d{2})/(?P<day>\d{2})/(?P<slug>[-\w]+)/$', DateDetailView.as_view(month_format='%m', model=Entry, date_field="pub_date"), name='entry_detail'),
     url(r'^tag/(?P<tag_name>[ \w]+)/$', views.get_entries_by_tag, name='tag'),
-	url(r'^category/(?P<category_name>[ \w]+)/$', views.get_entries_by_category, name='category'),
+    url(r'^category/(?P<category_name>[ \w]+)/$', views.get_entries_by_category, name='category'),
+    url(r'^home/$', views.index, name='home'),
+    url(r'^home/page/(?P<page_number>[0-9]+)/$', views.home, name='home'),
 ]
 
--- views.py
+++ views.py
@@ -1,18 +1,42 @@
+import math
+
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.http import Http404
 
 from .models import *
 # Create your views here.
 
 def index(request):
-    latest_entries = Entry.objects.order_by('-pub_time')[0:10]
-    tags = Tag.objects.all();
-    categories = Category.objects.all();
+    latest_entries = Entry.objects.order_by('-pub_time')[0:5]
+    tags = Tag.objects.all()
+    categories = Category.objects.all()
 	
     context = {
         'latest_entries': latest_entries,
         'tags': tags,
         'categories': categories,
+        'page_number': 1,
+    }
+    return render(request, 'weblog/home.html', context)
+
+def home(request, page_number):
+    entry_count = len(Entry.objects.all())
+    page_count = math.ceil(1.0*entry_count/5)
+    page_number = int(page_number)
+    if page_number < 1 or page_number > page_count:
+        raise Http404("invalid page number!")
+    lower = (page_number-1)*5
+    upper = lower+5
+    latest_entries = Entry.objects.order_by('-pub_time')[lower:upper]
+    tags = Tag.objects.all()
+    categories = Category.objects.all()
+
+    context = {
+        'latest_entries': latest_entries,
+        'tags': tags,
+        'categories': categories,
+        'page_number': page_number,
     }
     return render(request, 'weblog/home.html', context)
 
@@ -21,8 +45,8 @@ def article(request, year, month, day, slug):
 
 def get_entries_by_tag(request, tag_name):
     entries = Entry.objects.filter(tags__name=tag_name).order_by('-pub_time')[0:10]
-    tags = Tag.objects.filter(name=tag_name);
-    categories = Category.objects.all();
+    tags = Tag.objects.filter(name=tag_name)
+    categories = Category.objects.all()
     context = {
         'entries': entries,
         'tags': tags,
@@ -32,8 +56,8 @@ def get_entries_by_tag(request, tag_name):
 
 def get_entries_by_category(request, category_name):
     entries = Entry.objects.filter(categories__name=category_name).order_by('-pub_time')[0:10]
-    tags = Tag.objects.all();
-    categories = Category.objects.filter(name=category_name);
+    tags = Tag.objects.all()
+    categories = Category.objects.filter(name=category_name)
     context = {
         'entries': entries,
         'tags': tags,
